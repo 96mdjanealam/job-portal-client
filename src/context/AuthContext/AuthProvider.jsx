3cf1bd16b0d0813100183e8894a0b5c1d974e22e
96mdjanealam <96mdjanealam@gmail.com>
2024-12-20 12:17:02 +0600

    set loading placement update

--- src/context/AuthContext/AuthProvider.jsx
+++ src/context/AuthContext/AuthProvider.jsx
@@ -44,12 +44,26 @@ const AuthProvider = ({ children }) => {
 
       if (currentUser?.email) {
         const user = { email: currentUser.email };
-        axios.post("http://localhost:5000/jwt", user)
-        .then(res=> console.log(res.data))
+        axios
+          .post("http://localhost:5000/jwt", user, { withCredentials: true })
+          .then((res) => {
+            console.log("login token", res.data);
+            setLoading(false);
+          });
+      } else {
+        axios
+          .post(
+            "http://localhost:5000/logout",
+            {},
+            {
+              withCredentials: true,
+            }
+          )
+          .then((res) => {
+            console.log("logout", res.data);
+            setLoading(false);
+          });
       }
-
-      // put it in the right place
-      setLoading(false);
     });
 
     return () => {
